87255632250fd6121651bc7e1b636b50bce3e382
Erin Fajrin Nugraha <erinfajinnugraha@gmail.com>
2025-06-01 06:51:49 +0700

    perbaiki database

--- config/database.php
+++ config/database.php
@@ -1,18 +1,20 @@
 <?php
-$host = 'localhost';
-$dbname = 'safety-stock6';
-$username = 'root';
-$password = '';
+require_once __DIR__ . '/functions.php';
+
+
+$host     = $_ENV['DB_HOST'];
+$dbname   = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $conn = new mysqli($host, $username, $password, $dbname);
 
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
+
+    $conn->set_charset("utf8mb4");
 } catch (Exception $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-
-// Set charset
-$conn->set_charset("utf8mb4");
